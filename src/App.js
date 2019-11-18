1b3162f14d06f2984406033251f45fe3d19eb0d7
vahizurazov <vagiz.damir@gmail.com>
2019-11-18 13:49:39 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,125 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Board from "react-trello";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const data = {};
+const handleDragStart = (cardId, laneId) => {
+  console.log("drag started");
+  console.log(`cardId: ${cardId}`);
+  console.log(`laneId: ${laneId}`);
+};
+
+const handleDragEnd = (cardId, sourceLaneId, targetLaneId) => {
+  console.log("drag ended");
+  console.log(`cardId: ${cardId}`);
+  console.log(`sourceLaneId: ${sourceLaneId}`);
+  console.log(`targetLaneId: ${targetLaneId}`);
+};
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lanes: [
+        {
+          title: "Backlog",
+          id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(36),
+          cards: [
+            {
+              description: "111111111111111111",
+              title: "111111111111111111",
+              id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(
+                36
+              )
+            },
+            {
+              description: "222222222222222",
+              title: "222222222222222",
+              id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(
+                36
+              )
+            },
+            {
+              description: "33333333333333",
+              title: "33333333333333",
+              id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(
+                36
+              )
+            }
+          ]
+        },
+        {
+          title: "In Dev",
+          id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(36),
+          cards: [
+            {
+              description: "1 in DEV",
+              title: "1 in DEV",
+              id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(
+                36
+              )
+            },
+            {
+              description: "2 in DEV",
+              title: "2 in DEV",
+              id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(
+                36
+              )
+            }
+          ]
+        },
+        {
+          title: "Done",
+          id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(36),
+          cards: [
+            {
+              description: "1 DONE",
+              title: "1 DONE",
+              id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(
+                36
+              )
+            },
+            {
+              description: "2 DONE",
+              title: "2 DONE",
+              id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(
+                36
+              )
+            },
+            {
+              description: "3 DONE",
+              title: "3 DONE",
+              id: Math.floor(+new Date() + Math.random() * 0xffffffff).toString(
+                36
+              )
+            }
+          ]
+        }
+      ]
+    };
+  }
+  // handleCardAdd = (card, laneId) => {
+  //   console.log(`New card added to lane ${laneId}`);
+  //   console.dir(card);
+  // };
+  setEventBus = eventBus => {
+    console.log("eventBus", eventBus);
+    this.setState({ eventBus });
+  };
+
+  render() {
+    console.log("this.state", this.state);
+    console.log("handleDragStart", handleDragStart);
+    return (
+      <Board
+        data={this.state}
+        draggable
+        editable
+        onCardAdd={e => e}
+        // onCardAdd={this.handleCardAdd}
+        eventBusHandle={this.setEventBus}
+        handleDragStart={handleDragStart}
+        handleDragEnd={handleDragEnd}
+      />
+    );
+  }
 }
-
-export default App;
